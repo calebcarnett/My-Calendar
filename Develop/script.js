e5039b0d2c530157c4d6c98aeacc936393ad3628
calebcarnett <111102789+calebcarnett@users.noreply.github.com>
2022-11-11 14:26:57 -0500

    changed the conditional statements so it sorts through the id's and compares the correct time, added comments to the javascript

--- Develop/script.js
+++ Develop/script.js
@@ -1,57 +1,33 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
-
-
-
-var today = dayjs();
-$('#1a').text(today.format('MMM D, YYYY: h:mm A'));
-//This makes the hour variable only pull the hour and AM/PM
-var hour = today.format("hA");
-//This is grabbing the text out of the ID, I have the same ID set for all my time periods
-var time = $('#timeofday').text();
-
-//Confirming the two sets of data are the exact same
-console.log(hour);
-console.log(time);
-
-//Trying to run a function to loop through the texts in my #timeofday ID's and compare them. 
-time.each(function() {
-  if (hour === time) {
-    $('.time-block').addClass("present");
-  } else if (hour > time) {
-    $('.time-block').addClass("future");
-  } else (hour < time);  {
-    $('.time-block').addClass("past");
-  }
-});
-
-
-
-
-
-
-
-
-
+$(document).ready(function() {
+  var today = dayjs();
+  $("#1a").text(today.format("MMM D, YYYY: h:mm A"));
+  //This makes the hour variable only pull the hour and AM/PM
+  var hour = today.hour();
+  console.log(hour)
+  
+  var timeBlocks = $(".time-block");
+  //Changes the color of each time-block class depending on the hour of the day and if it matches the id within the class
+  timeBlocks.each(function () {
+    var actualTimeBlock = $(this).attr("id");
+    if (hour < actualTimeBlock) {
+      $(this).children(".col-md-10").attr("class", "col-8 col-md-10 description past")
+    } else if (hour > actualTimeBlock) {
+      $(this).children(".col-md-10").attr("class", "col-8 col-md-10 description future")
+    } else {
+      $(this).children(".col-md-10").attr("class", "col-8 col-md-10 description present")
+    }
+  });
+  
+  //Added a comment variable to grab the written text within the calendar and save it to the local storage
+  //Added a variable saveButton so that we can add an event listenter to save the text to local storage when clicked
+  var comment = document.querySelector(".description");
+  var saveButton = document.querySelector(".saveBtn");
+  
+  comment.innerHTML = localStorage.getItem("keyname");
+  //the comment.innerHTML lets me save down the "keyname" which is whatever the user inputs, to save even when refreshed. 
+  saveButton.addEventListener("click", function (event) {
+    localStorage.setItem("keyname", comment.value);
+    comment.innerHTML = localStorage.getItem("keyname");
+  });
+})
 
